67bf4e6b62c39570ecdf20496e34e250fd72046c
zjw <zhaojianwei@seo1158.com>
2020-05-12 17:57:06 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  base: '/',
+  dest: 'dist',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  extraWatchFiles: [//监听文件变动将会触发 vuepress 重新构建，并实时更新。
+    '../README.md'
+  ],
+  port: 3000,
+  theme: undefined,
+  markdown: {
+    lineNumbers: true //代码块显示行号
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': 'path/to/some/dir'
+      }
+    }
+  }
+}
